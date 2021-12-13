a7e3c82107f7ff757e3c65c5e3e51032be9fadd5
JBraunsmaJr <jonbraunsma@gmail.com>
2021-12-12 20:09:03 -0500

    Add project files.

---
+++ BrawnyFinal/ViewModels/ContactForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrawnyFinal.ViewModels;
+
+public class ContactForm
+{
+    [Required, EmailAddress] public string Email { get; set; }
+
+    [Required] public string Name { get; set; }
+
+    [Required] public string Message { get; set; }
+}
