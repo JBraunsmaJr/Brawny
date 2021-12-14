6eba5de40c89475227945adc09bc06c73f28c4f6
JBraunsmaJr <jonbraunsma@gmail.com>
2021-12-14 00:41:56 -0500

    Added page containing some of the resources I've accumulated over the years

---
+++ BrawnyFinal/Models/ResourceItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BrawnyFinal.Models;
+
+public class ResourceItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public string Type { get; set; }
+    public string[] Tags { get; set; }
+}
