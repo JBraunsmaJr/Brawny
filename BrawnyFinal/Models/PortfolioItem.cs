ed20824fa12b304f508980fa47c138d2421783a4
JBraunsmaJr <jonbraunsma@gmail.com>
2021-12-14 16:12:57 -0500

    Converted portfolio section into a json format - reducing copy/pasted structure. Added Brawny repo

---
+++ BrawnyFinal/Models/PortfolioItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BrawnyFinal.Models;
+
+public class PortfolioItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Github { get; set; }
+    public string Image { get; set; }
+}
